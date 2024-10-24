e41830131da8cf2776f88284650445f629a3fa6b
Amjad Althabteh <123609017+AmjadAlthabteh@users.noreply.github.com>
2024-10-24 14:03:53 -0400

    Update style.py

--- inventorytracker/style.py
+++ inventorytracker/style.py
@@ -1,16 +1,25 @@
+# style.py
 class Style:
-    # Colors
-    BG_GRADIENT = "#2C3E50"  # Dark blue-gray background for a gradient-like effect
-    TEXT_COLOR = "#ECF0F1"  # Light text color for contrast
-    ENTRY_BG = "#34495E"  # Darker gray for entry background
-    ENTRY_TEXT_COLOR = "#ECF0F1"  # Light text color for entries
-    BOX_COLOR = "#1ABC9C"  # Turquoise color for input field borders to make them stand out
-    BUTTON_BG = "#E67E22"  # Bright orange for buttons to catch attention
-    ORANGE_COLOR = "#F39C12"  # Rich orange color for the basketball icon and highlights
+    BG_COLOR = "#1C1C1C"  # dark black background
+    TEXT_COLOR = "#EAEAEA"  # light text color
+    ENTRY_BG = "#2C2C2C"  # drker gray for entry background
+    ENTRY_TEXT_COLOR = "#EAEAEA"  # light text for entries
+    BUTTON_COLOR = "#1ABC9C"  # teal color for buttons
+    BUTTON_HOVER = "#16A085"  # darker teal on hover
+    HIGHLIGHT_COLOR = "#F39C12"  # orange for highlights and labels
+    TEAM_COLORS = ["#F39C12", "#E74C3C", "#1ABC9C", "#3498DB", "#9B59B6"]  # Orange, Red, Teal, Blue, Purple
 
     # Fonts
-    TITLE_FONT_LARGE = ("Helvetica", 24, "bold")  # Large title font for the main heading
-    LABEL_FONT = ("Helvetica", 14, "bold")  # Font for labels to make them more readable
-    ENTRY_FONT = ("Helvetica", 12)  # Standard font for entry fields
-    BUTTON_FONT = ("Helvetica", 14, "bold")  # Bold font for buttons to make them stand out
+    FONT_TITLE = ("Poppins", 30, "bold")
+    FONT_LABEL = ("Poppins", 16)
+    FONT_ENTRY = ("Poppins", 14)
+    FONT_BUTTON = ("Poppins", 16, "bold")
+    FONT_CLOCK = ("Poppins", 18, "bold")
+
+    # =button Shape and Padding
+    BUTTON_PADX = 25  #  create circular button shape
+    BUTTON_PADY = 25
+    ENTRY_HEIGHT = 2
+    ENTRY_WIDTH = 20
+
 
